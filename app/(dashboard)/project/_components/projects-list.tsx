5aab3994a4cd73b97d39ac8c0ca9b225725ec7ed
sarah hesham <sarah.h.nashaat@gmail.com>
2026-04-19 17:51:18 +0200

    (fix): fix ui of project list with adding add project card at end

--- app/(dashboard)/project/_components/projects-list.tsx
+++ app/(dashboard)/project/_components/projects-list.tsx
@@ -4,6 +4,9 @@ import { projectsList } from "@/lib/types/projects.type";
 import EmptyState from "./empty-state";
 import ErrorState from "../../../../components/shared/error-state";
 import Header from "./header";
+import Button from "@/components/ui/button";
+import PlusIcon from "@/components/icons/plus-icon";
+import Link from "next/link";
 
 export default async function ProjectsList() {
   let projects: projectsList;
@@ -36,6 +39,20 @@ export default async function ProjectsList() {
             desc={project.description}
           />
         ))}
+        <div className="bg-white  p-6 rounded-lg min-h-55  border-dashed border-[#C3C6D633] border-2 flex justify-center items-center">
+          <Button
+            variant="ghost"
+            className="uppercase text-secondary text-sm font-bold flex flex-col gap-3.5"
+          >
+            <Link
+              className=" min-h-12 min-w-12 bg-surface-low  flex items-center justify-center rounded-xl"
+              href={"/project/add"}
+            >
+              <PlusIcon fill="black" />
+            </Link>
+            add project
+          </Button>
+        </div>
       </div>
     </>
   );
